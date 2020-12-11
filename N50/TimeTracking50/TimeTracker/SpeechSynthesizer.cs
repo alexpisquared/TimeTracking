bb88cd73318ca99ca0d6a76f728fb2a83dc833f8
AlexPi <alex.pigida@outlook.com>
2020-12-10 20:45:10 -0500

    better adapter placement

--- N50/TimeTracking50/TimeTracker/SpeechSynthesizer.cs
+++ N50/TimeTracking50/TimeTracker/SpeechSynthesizer.cs
@@ -1,19 +1,19 @@
-﻿using SpeechSynthLib;
-using System.Diagnostics;
-using System.Threading.Tasks;
+﻿//using SpeechSynthLib;
+//using System.Diagnostics;
+//using System.Threading.Tasks;
 
-namespace TimeTracker
-{
-  public class SpeechSynthesizer
-  {
-    SpeechSynth _synth;
+//namespace TimeTracker
+//{
+//  public class SpeechSynthesizer
+//  {
+//    SpeechSynth _synth;
 
-    public int Rate { get; set; }
-    public int Volume { get; set; }
+//    public int Rate { get; set; }
+//    public int Volume { get; set; }
 
-    public void SpeakAsyncCancelAll() => _synth = new SpeechSynth();
-    public void Speak(string msg) => _synth.SpeakAsync(msg).Wait();
-    public void SpeakAsync(string msg) => Task.Run(async () => await  _synth.SpeakAsync(msg));
-    public void SelectVoiceByHints(object gender) => Trace.WriteLine($" throw new NotImplementedException(); {gender}");
-  }
-}
+//    public void SpeakAsyncCancelAll() => _synth = new SpeechSynth();
+//    public void Speak(string msg) => _synth.SpeakAsync(msg).Wait();
+//    public void SpeakAsync(string msg) => Task.Run(async () => await  _synth.SpeakAsync(msg));
+//    public void SelectVoiceByHints(object gender) => Trace.WriteLine($" throw new NotImplementedException(); {gender}");
+//  }
+//}
