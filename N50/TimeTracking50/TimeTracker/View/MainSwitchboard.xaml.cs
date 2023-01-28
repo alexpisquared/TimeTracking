824dffac0ab78cb599d3cd8652db8cc1fd29bc04
AlexPi <alex.pigida@outlook.com>
2023-01-28 11:14:41 -0500

    Update MainSwitchboard.xaml.cs

--- N50/TimeTracking50/TimeTracker/View/MainSwitchboard.xaml.cs
+++ N50/TimeTracking50/TimeTracker/View/MainSwitchboard.xaml.cs
@@ -3,7 +3,6 @@ public partial class MainSwitchboard : AAV.WPF.Base.WindowBase
 {
   const int _zeroBasedBtnCnt = 4;
   bool _keepSaying;
-
   public MainSwitchboard(bool checkNewEmail, bool keepSaying, string? msg = null)
   {
     InitializeComponent();
@@ -33,7 +32,7 @@ public partial class MainSwitchboard : AAV.WPF.Base.WindowBase
       {
         while (_keepSaying)
         {
-          await App.SpeakAsync(msg ?? "Never mind!"); 
+          await App.SpeakAsync(msg ?? "Never mind!");
           await Task.Delay(15_000);
         }
 
@@ -45,9 +44,7 @@ public partial class MainSwitchboard : AAV.WPF.Base.WindowBase
 
     DataContext = this;
   }
-
-  DefaultSetting _settingS;   /**/ public DefaultSetting DSettngS { get => _settingS ??= A0DbContext.Create().DefaultSettings.FirstOrDefault(); set => _settingS = value; }
-
+  DefaultSetting? _settingS; public DefaultSetting DSettngS => _settingS ??= A0DbContext.Create().DefaultSettings.First(); 
   void setDf(int cb)
   {
     TS.IsDefault = PP.IsDefault = Ic.IsDefault = HR.IsDefault = AR.IsDefault = false;
@@ -61,25 +58,15 @@ public partial class MainSwitchboard : AAV.WPF.Base.WindowBase
       case 4: AR.IsDefault = true; break;
     }
   }
-
   protected override void OnClosed(EventArgs e) => base.OnClosed(e);
-
   void onClose(object? s, RoutedEventArgs? e) { Close(); Application.Current.Shutdown(); }
-
   void onTS(object s, RoutedEventArgs e) { pre(); _ = BindableBaseViewModel.ShowModalMvvm(new TimesheetPreviewVM(true), new TimeTracker.View.TimesheetPreview()); post(); }      //new TimeTracker.View.TimesheetPreview().ShowDialog();
-
   void onPP(object s, RoutedEventArgs e) { pre(); _ = new FromTillCtgrTaskNote().ShowDialog(); post(); }
-
   void onIc(object s, RoutedEventArgs e) { pre(); _ = new InvoicePreview(A0DbContext.Create()).ShowDialog(); post(); }
-
   void onHR(object s, RoutedEventArgs e) { pre(); { _ = MessageBox.Show("The feature is under contruction\n\nPlease come back soon", ((Button)s).Content.ToString()?.Replace("_", ""), MessageBoxButton.OK, MessageBoxImage.Information); } post(); }
-
   void onAR(object s, RoutedEventArgs e) { pre(); { _ = MessageBox.Show("The feature is under contruction\n\nPlease come back soon", ((Button)s).Content.ToString()?.Replace("_", ""), MessageBoxButton.OK, MessageBoxImage.Information); } post(); }
-
   void onST(object s, RoutedEventArgs e) { pre(); _ = new OptionsManager().ShowDialog(); post(); }
-
   void onDI(object s, RoutedEventArgs e) { pre(); TimeTrackDbCtx_Code1st_DbInitializer.DbIni(); post(); }      // NO GO at Wk&Hm????????????????                                                      //TimeTrackDbCtx_Simple__DbInitializer.DbIni();		// OK at Wk&Hm... but wrong dbx
-
   void onNavigate(object s, System.Windows.Navigation.RequestNavigateEventArgs e)
   {
     try
@@ -89,10 +76,7 @@ public partial class MainSwitchboard : AAV.WPF.Base.WindowBase
     }
     catch (Exception ex) { _ = ex.Log(); }
   }
-
   void pre() { _keepSaying = false; Hide(); }                                     //  ctrlPanelOnMarket.IsEnabled = false; WindowState = WindowState.Minimized; scrooves up focusing on the new window.   Hide(); - invokes Close */ }
-
   void post() { Bpr.BeepClk(); _ = new MainSwitchboard(false, false).ShowDialog(); }  //  ctrlPanelOnMarket.IsEnabled = true;  WindowState = WindowState.Normal; Show(); }//Task.Factory.StartNew(() => Thread.Sleep(100)).ContinueWith(_ => { Close(); }, TaskScheduler.FromCurrentSynchronizationContext()); }
-
   void wnd_Loaded(object sender, RoutedEventArgs e) => Bpr.BeepOk();
 }
