2cd916edacb836c5aabe6d9764f66a408bbca601
alexpisqueared <alex.pigida@outlook.com>
2024-03-30 10:27:12 -0400

    Updated logging, window behavior, and invoice PDF generation
    Updated `Tracer.cs` to change log path and commented out `DevOps.IsDbg` condition. In `WindowBase.cs`, removed the line that forced the window to stay on top when debugger is attached. Commented out `Trace_` method in `Bpr.cs` to stop logging certain parameters.
    
    In `InvoiceCreator.PDF`, added `System.IO` and `PdfSharp.Fonts` to the namespace, changed the font to "Segoe UI", and added a custom font resolver. Adjusted the height and radius of drawn ellipses, and changed the pen used for drawing. Updated labels to remove trailing colons and modified `SaveAndViewPdfFile` method to save the PDF document once and log any exceptions. Added `MyFontResolver` class to resolve font face and typeface.

--- N50/TimeTracking50/InvoiceCreator.PDF/InvoiceMaker.cs
+++ N50/TimeTracking50/InvoiceCreator.PDF/InvoiceMaker.cs
@@ -1,4 +1,7 @@
-﻿namespace InvoiceCreator.PDF;
+﻿using System.IO;
+using PdfSharp.Fonts;
+
+namespace InvoiceCreator.PDF;
 public class InvoiceMaker
 {
   const bool _includeBank = false;
@@ -23,7 +26,7 @@ public class InvoiceMaker
   {
     try
     {
-      const string fontname = "Segoe UI"; //  "Times New Roman";
+      const string fontname = "Segoe UI";
       const int lineHeight1 = 15, lineHeight2 = 24;
 
       var dark = XColors.Black;
@@ -31,6 +34,9 @@ public class InvoiceMaker
       var darkb = new XSolidBrush(dark);
       var liteb = new XSolidBrush(lite);
       var litep = new XPen(lite);
+
+      if (GlobalFontSettings.FontResolver == null) // Add a custom font resolver .. only once per run!
+        GlobalFontSettings.FontResolver = new MyFontResolver();
 
       _pdfDoc.Info.Title = "Created with PDFsharp";
 
@@ -44,13 +50,14 @@ public class InvoiceMaker
       var col3 = page.Width * .70;
       var colM = page.Width * .76;
       var col9 = page.Width * .90;
-      var h = 52.5;
+      var h = 50.5;
+      var pen0 = new XPen(lite, 1);
       var pen1 = new XPen(lite, .2);
       var pen2 = new XPen(dark, 2);
-      var rds = 3;
-      gfx.DrawEllipse(pen1, col1 + 00, h, rds, rds);
-      gfx.DrawEllipse(pen1, col1 + 20, h, rds, rds);
-      gfx.DrawEllipse(pen1, col1 + 40, h, rds, rds);
+      var rds = 5;
+      gfx.DrawEllipse(pen0, col1 + 01, h, rds, rds);
+      gfx.DrawEllipse(pen0, col1 + 21, h, rds, rds);
+      gfx.DrawEllipse(pen0, col1 + 41, h, rds, rds);
       gfx.DrawString("INVOICE", new XFont(fontname, 36, XFontStyleEx.Regular), liteb, col9, 40, XStringFormats.TopRight);
       h = 85d;
       gfx.DrawLine(litep, col1, h, col9, h);
@@ -62,14 +69,14 @@ public class InvoiceMaker
       var fontNndl = new XFont(fontname, 11, XFontStyleEx.Underline);
 
       h = page.Height * .150;
-      h += lineHeight2; gfx.DrawString("     Invoice #:   ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight); gfx.DrawString($"{invcNumber}          ", fontValu, darkb, col2, h, XStringFormats.BaseLineLeft);
-      h += lineHeight2; gfx.DrawString("          Date:   ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight); gfx.DrawString($"{invcDate:dd-MMM-yyyy}", fontValu, darkb, col2, h, XStringFormats.BaseLineLeft);
-      h += lineHeight2; gfx.DrawString("For the period:   ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight); gfx.DrawString($"{pyPeriod}            ", fontValu, darkb, col2, h, XStringFormats.BaseLineLeft);
+      h += lineHeight2; gfx.DrawString("     Invoice #    ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight); gfx.DrawString($"{invcNumber}          ", fontValu, darkb, col2, h, XStringFormats.BaseLineLeft);
+      h += lineHeight2; gfx.DrawString("          Date    ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight); gfx.DrawString($"{invcDate:dd-MMM-yyyy}", fontValu, darkb, col2, h, XStringFormats.BaseLineLeft);
+      h += lineHeight2; gfx.DrawString("For the period    ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight); gfx.DrawString($"{pyPeriod}            ", fontValu, darkb, col2, h, XStringFormats.BaseLineLeft);
 
       h = page.Height * .320;
       gfx.DrawString(coName1, fontCoNm, darkb, col1, h, XStringFormats.BaseLineLeft);
       gfx.DrawString(coName2, fontCoNm, darkb, col2, h, XStringFormats.BaseLineLeft);
-      gfx.DrawString("Invoice to:   ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight);
+      gfx.DrawString("Invoice to    ", fontLabl, liteb, col2, h, XStringFormats.BaseLineRight);
 
       printLines(coAdrs1, lineHeight1, fontname, darkb, gfx, col1, h);
       printLines(coAdrs2, lineHeight1, fontname, darkb, gfx, col2, h);
@@ -83,11 +90,11 @@ public class InvoiceMaker
       gfx.DrawString(ratePerHr, fontValu, darkb, page.Width * .62, h, XStringFormats.BaseLineRight);
       gfx.DrawString(total_ttl, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
 
-      h += lineHeight2; gfx.DrawString("  Subtotal:", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(sub_total, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
-      h += lineHeight2; gfx.DrawString("  Tax Rate:", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(taxRatePc, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
-      h += lineHeight2; gfx.DrawString(" Sales Tax:", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(salesTaxM, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
-      h += lineHeight2; gfx.DrawString("     Other:", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight);
-      h += lineHeight2; gfx.DrawString("     Total:", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(grandtotl, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
+      h += lineHeight2; gfx.DrawString("  Subtotal ", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(sub_total, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
+      h += lineHeight2; gfx.DrawString("  Tax Rate ", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(taxRatePc, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
+      h += lineHeight2; gfx.DrawString(" Sales Tax ", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(salesTaxM, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
+      h += lineHeight2; gfx.DrawString("     Other ", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight);
+      h += lineHeight2; gfx.DrawString("     Total ", fontLabl, liteb, colM, h, XStringFormats.BaseLineRight); gfx.DrawString(grandtotl, fontValu, darkb, col9, h, XStringFormats.BaseLineRight);
 
       h = page.Height * .52; gfx.DrawLine(pen1, col1, h, col9, h);
       h += lineHeight2; gfx.DrawLine(pen1, col1, h, col9, h);
@@ -103,9 +110,9 @@ public class InvoiceMaker
       if (_includeBank)
       {
         gfx.DrawString("Bank Account Details", fontLabB, liteb, colA, h, XStringFormats.BaseLineLeft);                //gfx.DrawString($"{invcNumber}          ", fontValu, darkb, col2, h, XStringFormats.BaseLineLeft);
-        h += lineHeight2; gfx.DrawString("          Bank:   ", fontLabl, liteb, colB, h, XStringFormats.BaseLineRight); gfx.DrawString("TD Canada Trust", fontValu, darkb, colB, h, XStringFormats.BaseLineLeft);
-        h += lineHeight2; gfx.DrawString("   Branch code:   ", fontLabl, liteb, colB, h, XStringFormats.BaseLineRight); gfx.DrawString("03212          ", fontValu, darkb, colB, h, XStringFormats.BaseLineLeft);
-        h += lineHeight2; gfx.DrawString("Account Number:   ", fontLabl, liteb, colB, h, XStringFormats.BaseLineRight); gfx.DrawString("5999741        ", fontValu, darkb, colB, h, XStringFormats.BaseLineLeft);
+        h += lineHeight2; gfx.DrawString("          Bank    ", fontLabl, liteb, colB, h, XStringFormats.BaseLineRight); gfx.DrawString("TD Canada Trust", fontValu, darkb, colB, h, XStringFormats.BaseLineLeft);
+        h += lineHeight2; gfx.DrawString("   Branch code    ", fontLabl, liteb, colB, h, XStringFormats.BaseLineRight); gfx.DrawString("03212          ", fontValu, darkb, colB, h, XStringFormats.BaseLineLeft);
+        h += lineHeight2; gfx.DrawString("Account Number    ", fontLabl, liteb, colB, h, XStringFormats.BaseLineRight); gfx.DrawString("5999741        ", fontValu, darkb, colB, h, XStringFormats.BaseLineLeft);
       }
       else
       {
@@ -120,12 +127,14 @@ public class InvoiceMaker
     catch (Exception ex) { _ = ex.Log(); }
   }
 
-  public async void SaveAndViewPdfFile(string pdfFilename)
+  public void SaveAndViewPdfFile(string pdfFilename)
   {
-    for (var i = 0; i < 10; i++)
-      try { _pdfDoc.Save(pdfFilename); } catch (Exception ex) { _ = ex.Log(pdfFilename); Console.Beep(333 + (i * 10), i * 100); await Task.Delay(999); }
-
-    try { _ = Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", $"\"{pdfFilename}\""); } catch (Exception ex2) { _ = ex2.Log(pdfFilename); }
+    try
+    {
+      _pdfDoc.Save(pdfFilename);
+      _ = Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", $"\"{pdfFilename}\"");
+    }
+    catch (Exception ex2) { _ = ex2.Log(pdfFilename); throw; }
   }
 
   static void printLines(string coAdrs1, int lineHeight1, string fontname, XSolidBrush dark, XGraphics gfx, double x, double h)
@@ -136,10 +145,33 @@ public class InvoiceMaker
       h += lineHeight1; gfx.DrawString(line, font, dark, x, h, XStringFormats.BaseLineLeft);
     }
   }
+
+  // Custom font resolver class
+  public class MyFontResolver : IFontResolver
+  {
+    public byte[] GetFont(string faceName)
+    {
+      using (var ms = new MemoryStream())
+      {
+        // Replace the path with the actual path to the Arial font file
+        using (var fs = File.OpenRead(@"C:\Windows\Fonts\segoeui.ttf"))
+        {
+          fs.CopyTo(ms);
+        }
+        return ms.ToArray();
+      }
+    }
+
+    public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+    {
+      // Return the font family name and style
+      return new FontResolverInfo("Arial", isBold, isItalic);
+    }
+  }
 }
 /*
-                Bank: TD Canada Trust
-         Branch code: 03212
-      Account Number: 5999741
-  Designation Number: 7919
+                Bank  TD Canada Trust
+         Branch code  03212
+      Account Number  5999741
+  Designation Number  7919
 */
